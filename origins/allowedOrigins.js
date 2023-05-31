6f17839b63da70e0d1022046bfcfc44ad1b3da5d
Kislame <shadoooo198@gmail.com>
2023-05-31 20:11:38 +0200

    fixed allowed origins

--- origins/allowedOrigins.js
+++ origins/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  'https://ecommerce-app.onrender.com',
-  'https://ecommerce-admin.onrender.com',
+  'https://ecommerce-admin-xiw5.onrender.com',
+  'https://ecommerce-app-bmlr.onrender.com',
 ];
 
 module.exports = allowedOrigins;
