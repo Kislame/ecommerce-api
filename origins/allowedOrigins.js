88c21b58891896a1f59cce3d4c13611c96b5f065
Kislame <shadoooo198@gmail.com>
2024-02-05 21:58:46 +0100

    update origins

--- origins/allowedOrigins.js
+++ origins/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   'https://ecommerce-admin-xiw5.onrender.com',
   'https://ecommerce-app-bmlr.onrender.com',
+  'https://ecommerce-app-kappa-seven.vercel.app/',
+  'http://127.0.0.1:5173',
 ];
 
 module.exports = allowedOrigins;
