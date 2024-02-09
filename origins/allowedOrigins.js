2e2713eb3855ddcd4bb96dec35c77ed5902dc809
Kislame <shadoooo198@gmail.com>
2024-02-09 21:01:29 +0100

    added origin

--- origins/allowedOrigins.js
+++ origins/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   'https://ecommerce-admin-xiw5.onrender.com',
   'https://ecommerce-app-bmlr.onrender.com',
   'https://ecommerce-app-kappa-seven.vercel.app/',
-  'http://127.0.0.1:5173',
+  'http://localhost:5173/',
 ];
 
 module.exports = allowedOrigins;
